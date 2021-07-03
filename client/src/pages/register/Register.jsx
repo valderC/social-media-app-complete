05745a272b34f011f9bbe5c30dd2838126931301
yurik <yurik.valder@gmail.com>
2021-07-03 00:45:27 -0400

    fixing

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -34,9 +34,9 @@ export default function Register() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Valdersocial</h3>
+          <h3 className="loginLogo">ValderSocial</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
+            Connect with friends and the world around you.
           </span>
         </div>
         <div className="loginRight">
